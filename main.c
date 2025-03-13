2112afbe4b633f6f901394464d3b106be9d46f43
Ivan <ivan.kyspys@gmail.com>
2025-03-13 16:57:39 +0200

    Add send_packet and recv_packet functions for sending/receiving ICMP packets

--- main.c
+++ main.c
@@ -21,7 +21,28 @@ int create_socket(char* dest_IP_str, struct sockaddr_in* dest_addr, int dest_add
 	dest_addr->sin_port = 0;
 
         return sock_fd;
-}	
+}
+
+void send_packet(int sock_fd, const void* packet, int packet_size, struct sockaddr* dest, int dest_size)
+{
+        int data_sent = sendto(sock_fd, packet, packet_size, 0, (struct sockaddr *)dest, dest_size);
+        if(data_sent < 0)
+        {
+                perror("Error: sending data failed.");
+                exit(1);
+        }
+}
+
+void recv_packet(int sock_fd, char* rcvd_msg, int rcvd_msg_size, struct sockaddr* node_IP, int* node_IP_size)
+{
+        int data_recvd = recvfrom(sock_fd, rcvd_msg, rcvd_msg_size, 0, node_IP, node_IP_size);
+        if(data_recvd < 0)
+        {
+                perror("Error: receiving data failed.");
+                exit(1);
+        }
+}
+
 int main(int argc, char* argv[])
 {
         // TODO: implement main programm logic.
